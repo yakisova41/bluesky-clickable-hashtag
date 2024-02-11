524ee6b00cf8dc7e26d3e40ddd9702a8dfcd0848
yakisova41 <yakisoba5emon@gmail.com>
2024-02-11 18:08:31 +0000

    💄 fix tag style

--- extension/userscript.user.js
+++ extension/userscript.user.js
@@ -16,19 +16,40 @@ setTimeout(() => {
     const routerElem = document.querySelector("#root > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) ")
     const navigation = routerElem[getPropsKey(routerElem)].children.props.children[0].props.children.props.children.props.children.props.value.navigation;
 
+    const style = document.createElement("style");
+    style.textContent = `
+        .clickable-hashtag-link {
+            background-color: rgba(0, 0, 0, 0);
+            border: 0px solid black;
+            box-sizing: border-box;
+            color: rgb(0, 0, 0);
+            display: inline;
+            font: 14px -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            list-style: none;
+            margin: 0px;
+            padding: 0px;
+            position: relative;
+            text-align: start;
+            text-decoration: none;
+            white-space: pre-wrap;
+            overflow-wrap: break-word;
+        }
+    `;
+    document.head.appendChild(style);
+
     const observer = new MutationObserver(()=> {    
         const texts = document.querySelectorAll(`div[data-word-wrap="1"]:not(.hashtaged)`);
         texts.forEach(textElem => {
             textElem.classList.add("hashtaged");
-            textElem.innerHTML = textElem.innerHTML.replace(/#(\S*)/g, '<a class="hashtag-adding" keyword="$1" style="letter-spacing: 0.25px; font-weight: 400; color: rgb(16, 131, 254);" href="/search?q=$1">#$1</a>')
-            const adding = textElem.querySelectorAll(".hashtag-adding");
-            adding.forEach(addingElem => {
-                addingElem.addEventListener("click", (e) => {
+            textElem.innerHTML = textElem.innerHTML.replace(/#(\S*)/g, '<a class="clickable-hashtag-nolistener clickable-hashtag-link" role="link" keyword="$1" style="font-size: 16px; letter-spacing: 0.25px; font-weight: 400; color: rgb(16, 131, 254);" href="/search?q=$1">#$1</a>')
+            const nolistenerElements = textElem.querySelectorAll(".clickable-hashtag-nolistener");
+            nolistenerElements.forEach(nolistenerElement => {
+                nolistenerElement.addEventListener("click", (e) => {
                     e.stopPropagation()
                     e.preventDefault()
-                    navigation.push("Search", {q: addingElem.getAttribute("keyword")})
+                    navigation.push("Search", {q: nolistenerElement.getAttribute("keyword")})
                 });
-                addingElem.classList.remove("hashtag-adding");            
+                nolistenerElement.classList.remove("clickable-hashtag-nolistener");            
             })
         });
     });
